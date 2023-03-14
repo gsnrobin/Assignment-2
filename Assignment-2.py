9be1efc48f8b6f5d4189d4a3d254136fd10cd560
Golam shah newaz Robi <105287720+gsnrobin@users.noreply.github.com>
2023-03-14 15:30:45 +0600

    Add files via upload

---
+++ Assignment-2.py
@@ -0,0 +1,39 @@
+Assignment-2
+
+1. If you want to see the disk usage of your linux system which command you run ?
+Ans : IF we want to see the disk usage of my linux system i need to used df -h
+
+2.  If you want to see memery usage of your linux system which command you run ?
+Ans : IF we want to see the memery usage of my linux system i need to used free -m
+
+3. If you want to see the running process of your linux system which command you run ?
+Ans :IF we want to see the running process of my linux system i need to used  top command.
+
+4. If you want kill any process , What is the command?
+Ans: if we want kill any process we need to used Kill process_name 
+
+5. What does rwx & UGOmeaning in file permission and ownership?
+Ans :
+rwx is the file  Permission Type,
+r means to read. It is denoted by ‘r’ when a character defines it, and it is characterized by 4 when a number defines it. which user has read permission he can read any file or folder only
+W means  writer. It is denoted by ‘w’ when the character defines it, and it is characterized by 2.
+X means execute. is denoted by ‘x’ when the character defines it, and it is characterized by 1 when the number defines it.
+
+UGO is ownership type,
+U means User. It is denoted by ‘U',
+the user can be one user or multiuser, but all users do not same permission in the system.
+
+G means Group, It is denoted by ‘G’.this section can have single group or a multigroup, when to give any permission to any group, then which users are members of this group all users get the same permission.
+and O means other. It is denoted by ‘O’
+
+
+6. What permission define in the 664 ?
+Ans: The 665 define user read and write permission ,group read and write permission and other only read premission.
+7. Write the name of file ownership attribute ?
+Ans:
+UGO is ownership type,
+U means User. It is denoted by ‘U',
+the user can be one user or multiuser, but all users do not same permission in the system.
+
+G means Group, It is denoted by ‘G’.this section can have single group or a multigroup, when to give any permission to any group, then which users are members of this group all users get the same permission.
+and O means other. It is denoted by ‘O’
